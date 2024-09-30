6281e69ae1403e659cafc3850729173d6e868aab
Somyacoder <165130748+Somyadubey1598@users.noreply.github.com>
2024-09-30 14:46:52 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,10 +1,8 @@
 class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+    def romanToInt(self, s:str)-> int:
+                   d={'I':1,'v': 5, 'X' : 10, 'L': 50, 'c':100, 'D': 500, 'M': 1000}
+                      return sum ((-1 if d[a] < d[b] else 1) * d[a] for a, b in pair wise(s)) + d[s[-1]]
+        
 
 
 
